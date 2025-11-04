1bb34b83f4293bfd822baebe7b00ad8419055bd5
ZakharovaValeria <Valeria122610Z@yandex.ru>
2025-11-04 16:02:46 +0500

    Task2

---
+++ Tyuiu.ZakharovaVI.Sprint4.Task2.V5.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ZakharovaVI.Sprint4.Task2.V5.Lib
+{
+    public class DataService : ISprint4Task2V5
+    {
+        public int Calculate(int[] array)
+        {
+            int umn = 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    umn *= array[i];
+                }
+            }
+            return umn;
+        }
+    }
+}
---
+++ Tyuiu.ZakharovaVI.Sprint4.Task2.V5/Program.cs
@@ -0,0 +1,56 @@
+﻿using Tyuiu.ZakharovaVI.Sprint4.Task2.V5.Lib;
+
+namespace Tyuiu.ZakharovaVI.Sprint4.Task2.V5
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Захарова В. И. | ИБКС-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы. (генератор случайных чисел)                   *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнила: Захарова Валерия Игоревна | ИБКСб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 14 элементов                     *");
+            Console.WriteLine("* подсчитать произведение четных элементов массива.                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите количество элементов массива: ");
+            int len = Convert.ToInt32(Console.ReadLine());
+
+            int[] numsArray = new int[len];
+
+             for (int i = 0; i <= len - 1; i++)
+            {
+                numsArray[i] = rnd.Next(3, 9);
+            }
+
+            Console.Write("Массив:");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.Write(numsArray[i] + "\t");
+            }
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(numsArray);
+            Console.WriteLine("Произведение четных элементов:" + res);
+
+            Console.ReadKey();
+        }
+    }
+}
