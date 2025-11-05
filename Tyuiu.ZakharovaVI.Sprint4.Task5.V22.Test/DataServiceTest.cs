cfcad182981f590ef0013c95952df0a745ed4cd7
ZakharovaValeria <Valeria122610Z@yandex.ru>
2025-11-05 23:52:13 +0500

    Task5

---
+++ Tyuiu.ZakharovaVI.Sprint4.Task5.V22.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.ZakharovaVI.Sprint4.Task5.V22.Lib;
+
+namespace Tyuiu.ZakharovaVI.Sprint4.Task5.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = new int[5, 5] { { -6, -5, 1, 2, 3 }, { -6, -1, 7, 2, 4 }, { -2, -5, -7, 1, 2 }, { 1, 4, 5, 6, 7 }, { -3, -2, -1, 5, 3 } };
+            int res = ds.Calculate(mas2);
+            int wait = 10;
+
+            Assert.AreEqual(wait, res);
+
+
+        }
+    }
+}
---
+++ Tyuiu.ZakharovaVI.Sprint4.Task5.V22/Program.cs
@@ -0,0 +1,68 @@
+﻿using Tyuiu.ZakharovaVI.Sprint4.Task5.V22.Lib;
+
+namespace Tyuiu.ZakharovaVI.Sprint4.Task5.V22
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+            Random rnd = new Random();
+            Console.Title = "Спринт #4 | Выполнила: Захарова В. И. | ИБКС-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (генератор случайных чисел)                    *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнила: Захарова Валерия Игоревна | ИБКСб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов                     *");
+            Console.WriteLine("* Найти количество отрицательных элементов.                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите количество строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите количество столбцов в массиве: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix = new int[rows, columns];
+
+
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i,j] = rnd.Next(-8, 9);
+                }
+            }
+
+            Console.WriteLine("\nМассив");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{matrix[i,j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(matrix);
+            Console.WriteLine("Количество нулевых элементов = " + res);
+            Console.ReadKey();
+        }
+    }
+}
