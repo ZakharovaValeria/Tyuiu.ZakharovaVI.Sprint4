2f66832086b90652f01123ad6f12c1522c401d8b
ZakharovaValeria <Valeria122610Z@yandex.ru>
2025-11-06 01:38:55 +0500

    Task7

---
+++ Tyuiu.ZakharovaVI.Sprint4.Task7.V1.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.ZakharovaVI.Sprint4.Task7.V1.Lib;
+
+namespace Tyuiu.ZakharovaVI.Sprint4.Task7.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int n = 3;
+            int m = 3;
+            int[,] mtrx = new int[n, m];
+            string str = "135792468";
+            DataService ds = new DataService();
+            int res = ds.Calculate(n, m, str);
+            int wait = 4;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
+ 
---
+++ Tyuiu.ZakharovaVI.Sprint4.Task7.V1/Program.cs
@@ -0,0 +1,70 @@
+﻿using Tyuiu.ZakharovaVI.Sprint4.Task7.V1.Lib;
+
+namespace Tyuiu.ZakharovaVI.Sprint4.Task7.V1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            int m = 3;
+            int[,] mtrx = new int[n, m];
+            int index = 0;
+            string str = "135792468"; 
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Захарова В. И. | ИБКС-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнила: Захарова Валерия Игоревна | ИБКСб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр 135792468                             *");
+            Console.WriteLine("* Преобразуйте ее в матрицу 3 на 3 и подсчитайте количество четных чисел. *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            //int index = 0;
+
+            Console.WriteLine("\nМассив: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+
+            
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < m; j++)
+            //    {
+            //        Console.WriteLine($"{str[index]} \t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(n, m, str);
+
+
+            Console.WriteLine("Количество четных чисел: " + res);
+            Console.ReadKey();
+        }
+    }
+}
